6b6e982db725593d76a7200bdff5fe1f840c7af6
drpythonjavascript <drpythonjavascript@gmail.com>
2022-03-21 08:33:12 -0400

    initial commit

---
+++ afrex/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          ** <code>Fullstack Projects (MERN or MEAN or MEVN or  PERN or FARM Stack</code> Mocha (Testing Framework) and chai(assertion Library) for TTD unit testing.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Full Stack Development With BlackJavaScript
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
